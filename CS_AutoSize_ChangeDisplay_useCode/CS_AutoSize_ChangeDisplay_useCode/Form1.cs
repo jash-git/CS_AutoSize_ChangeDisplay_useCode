f8821c00efc819f22e4e2e513bc4a5f732630c51
jash-git <jash.liao@gmail.com>
2016-12-29 17:20:40 +0800

    2016/12/29 Create

---
+++ CS_AutoSize_ChangeDisplay_useCode/CS_AutoSize_ChangeDisplay_useCode/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CS_AutoSize_ChangeDisplay_useCode
+{
+    public partial class Form1 : Form
+    {
+        public Display_API m_Display_API;
+        //--
+        //step_01
+        private float m_fltResolution_W = 1920;//系統預設解析度-寬度
+        private float m_fltResolution_H = 1080;//系統預設解析度-高度
+        private float m_fltRFactor_W = 1;//解析度放大倍率因子-寬度
+        private float m_fltRFactor_H = 1;//解析度放大倍率因子-高度
+        
+        private float m_fltDpi_W = 96;//預設DPI-寬度
+        private float m_fltDpi_H = 96;//預設DPI-高度
+        private float m_fltDFactor_W = 1;//DPI放大倍率因子-寬度
+        private float m_fltDFactor_H = 1;//DPI放大倍率因子-高度
+
+        private float m_fltSysFactor_W = 1;
+        private float m_fltSysFactor_H = 1;
+
+        
+        private Single m_currentSiz;
+        //--
+        public Form1()
+        {
+            InitializeComponent();
+            showMsg();
+            setTag(this);
+            setControls_Position_Size((1 / m_fltSysFactor_W), (1 / m_fltSysFactor_H), this);
+            this.Width = (int)(this.Width * (1 / m_fltSysFactor_W));
+            this.Height = (int)(this.Height * (1 / m_fltSysFactor_W));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        //--
+        private void setTag(Control cons)
+        {
+            //string data = cons.Tag.ToString();
+            //string[] mytag = cons.Tag.ToString().Split(new char[] { ':' });//獲取控制項的Tag屬性值，並分割後存儲字元串數組
+            //m_currentSiz = System.Convert.ToSingle(mytag[4]);
+
+            foreach (Control con in cons.Controls)
+            {
+                con.Tag = con.Width + ":" + con.Height + ":" + con.Left + ":" + con.Top + ":" + con.Font.Size;
+                m_currentSiz = con.Font.Size;
+                if (con.Controls.Count > 0)
+                    setTag(con);
+            }
+        }
+        private void setControls_Position_Size(float newx, float newy, Control cons)
+        {
+            //遍歷窗體中的控制項，重新設置控制項的值
+            foreach (Control con in cons.Controls)
+            {
+                richTextBox1.Text += "\n--------------------\n";
+                string data = con.Tag.ToString();
+                string[] mytag = con.Tag.ToString().Split(new char[] { ':' });//獲取控制項的Tag屬性值，並分割後存儲字元串數組
+                float a = System.Convert.ToSingle(mytag[0]) * newx;//根據窗體縮放比例確定控制項的值，寬度
+                richTextBox1.Text += "\n元件原本寬度 = " + System.Convert.ToSingle(mytag[0])+"\t";
+                con.Width = (int)a;//寬度
+                richTextBox1.Text += "元件之後寬度 = " + con.Width + "\n";
+                a = System.Convert.ToSingle(mytag[1]) * newy;//高度
+                richTextBox1.Text += "元件原本高度 = " + System.Convert.ToSingle(mytag[1]) + "\t";
+                con.Height = (int)(a);
+                richTextBox1.Text += "元件之後高度 = " + con.Height + "\n";
+                a = System.Convert.ToSingle(mytag[2]) * newx;//左邊距離
+                richTextBox1.Text += "元件原本X = " + System.Convert.ToSingle(mytag[2]) + "\t";
+                con.Left = (int)(a);
+                richTextBox1.Text += "元件之後X = " + con.Left + "\n";
+                a = System.Convert.ToSingle(mytag[3]) * newy;//上邊緣距離
+                richTextBox1.Text += "元件原本Y = " + System.Convert.ToSingle(mytag[3]) + "\t";
+                con.Top = (int)(a);
+                richTextBox1.Text += "元件之後Y = " + con.Top + "\n";
+                Single currentSize = System.Convert.ToSingle(mytag[4]) * newy;//字體大小
+                richTextBox1.Text += "元件原本字型大小 = " + System.Convert.ToSingle(mytag[4]) + "\t";
+                con.Font = new Font(con.Font.Name, currentSize, con.Font.Style, con.Font.Unit);
+                richTextBox1.Text += "元件之後字型大小 = " + currentSize + "\t";
+                if (con.Controls.Count > 0)
+                {
+                    setControls_Position_Size(newx, newy, con);
+                }
+            }
+        }
+        //--
+        //--
+        private void showMsg()
+        {
+            m_Display_API = new Display_API();
+            m_Display_API.getDisplaySetting();
+            richTextBox1.Text = "\n";
+
+            richTextBox1.Text += "預設螢幕解析度 = " + m_fltResolution_W + " * " + m_fltResolution_H + "\n";
+            richTextBox1.Text += "目前系統解析度 = " + m_Display_API.m_intWidth + " * " + m_Display_API.m_intHeight + "\n";
+            m_fltRFactor_W = m_fltResolution_W / m_Display_API.m_intWidth;//計算解析度放大倍率因子-寬度
+            m_fltRFactor_H = m_fltResolution_H / m_Display_API.m_intHeight;//計算解析度放大倍率因子-高度
+            richTextBox1.Text += "目前解析度放大倍率因子 = " + m_fltRFactor_W + " * " + m_fltRFactor_H + "\n";
+            richTextBox1.Text += "\n--------------------\n";
+
+            richTextBox1.Text += "\n目前系等本身放大因子(實際解析度/邏輯解析度) = " + m_Display_API.m_fltScreenScalingFactor_W + " * " + m_Display_API.m_fltScreenScalingFactor_H + "\n";
+            richTextBox1.Text += "\n--------------------\n";
+
+            Graphics graphics = this.CreateGraphics();
+            float fltDpi_W, fltDpi_H;
+            fltDpi_W = graphics.DpiX;
+            fltDpi_H = graphics.DpiY;
+            richTextBox1.Text += "\n預設螢幕Dpi = " + m_fltDpi_W + " * " + m_fltDpi_H + "\n";
+            richTextBox1.Text += "目前系統Dpi = " + fltDpi_W + " * " + fltDpi_H + "\n";
+            m_fltDFactor_W = fltDpi_W / m_fltDpi_W;//計算DPI度放大倍率因子-寬度
+            m_fltDFactor_H = fltDpi_H / m_fltDpi_H;//計算DPI放大倍率因子-高度
+            richTextBox1.Text += "目前Dpi放大倍率因子 = " + m_fltDFactor_W + " * " + m_fltDFactor_H + "\n";
+            richTextBox1.Text += "\n--------------------\n";
+
+            m_fltSysFactor_W = m_fltRFactor_W * m_Display_API.m_fltScreenScalingFactor_W * m_fltDFactor_W;
+            m_fltSysFactor_H = m_fltRFactor_H * m_Display_API.m_fltScreenScalingFactor_H * m_fltDFactor_H;
+            richTextBox1.Text += "\n目前放大倍率因子(Sys) = " + m_fltSysFactor_W + " * " + m_fltSysFactor_H + "\n";
+
+
+        }
+        //--
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ CS_AutoSize_ChangeDisplay_useCode/CS_AutoSize_ChangeDisplay_useCode/Lib/Display_API.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//--
+//step_01
+using System.Runtime.InteropServices;
+using System.Drawing;
+//-
+/*Auto Size in responce to the Display Settings*/
+namespace CS_AutoSize_ChangeDisplay_useCode
+{
+    public class Display_API
+    {
+        //--
+        //step_02
+        public enum DMDO
+        {
+            DEFAULT = 0,
+            D90 = 1,
+            D180 = 2,
+            D270 = 3
+        }
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        struct DEVMODE
+        {
+            public const int DM_DISPLAYFREQUENCY = 0x400000;
+            public const int DM_PELSWIDTH = 0x80000;
+            public const int DM_PELSHEIGHT = 0x100000;
+            private const int CCHDEVICENAME = 32;
+            private const int CCHFORMNAME = 32;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHDEVICENAME)]
+            public string dmDeviceName;
+            public short dmSpecVersion;
+            public short dmDriverVersion;
+            public short dmSize;
+            public short dmDriverExtra;
+            public int dmFields;
+            public int dmPositionX;
+            public int dmPositionY;
+            public DMDO dmDisplayOrientation;
+            public int dmDisplayFixedOutput;
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHFORMNAME)]
+            public string dmFormName;
+            public short dmLogPixels;
+            public int dmBitsPerPel;
+            public int dmPelsWidth;
+            public int dmPelsHeight;
+            public int dmDisplayFlags;
+            public int dmDisplayFrequency;
+            public int dmICMMethod;
+            public int dmICMIntent;
+            public int dmMediaType;
+            public int dmDitherType;
+            public int dmReserved1;
+            public int dmReserved2;
+            public int dmPanningWidth;
+            public int dmPanningHeight;
+        }
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int ChangeDisplaySettings([In] ref DEVMODE lpDevMode, int dwFlags);
+        //--
+
+        //--
+        //step_03
+        private void changeDisplaySetting(int intWidth, int intHeight, int intFrequency)
+        {
+            long RetVal = 0;
+            DEVMODE dm = new DEVMODE();
+            dm.dmSize = (short)Marshal.SizeOf(typeof(DEVMODE));
+            dm.dmPelsWidth = intWidth;
+            dm.dmPelsHeight = intHeight;
+            dm.dmDisplayFrequency = intFrequency;
+            dm.dmFields = DEVMODE.DM_PELSWIDTH | DEVMODE.DM_PELSHEIGHT | DEVMODE.DM_DISPLAYFREQUENCY;
+            RetVal = ChangeDisplaySettings(ref dm, 0);
+        }
+        public int m_intWidth = -1;
+        public int m_intHeight = -1;
+        public float m_fltScreenScalingFactor_H=0.0f;
+        public float m_fltScreenScalingFactor_W = 0.0f;
+        //--
+
+        //--
+        //step_04
+        [DllImport("gdi32.dll")]
+        static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+        public enum DeviceCap
+        {
+            HORZRES = 8,
+            VERTRES = 10,
+            DESKTOPVERTRES = 117,
+            DESKTOPHORZRES = 118,
+
+            // http://pinvoke.net/default.aspx/gdi32/GetDeviceCaps.html
+        }
+
+        public void getDisplaySetting()
+        {
+            m_intWidth = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+            m_intHeight = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+            getScalingFactor();
+
+        }
+        public void getScalingFactor()//放大比例
+        {
+            Graphics g = Graphics.FromHwnd(IntPtr.Zero);
+            IntPtr desktop = g.GetHdc();
+            int LogicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.VERTRES);
+            int PhysicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPVERTRES);
+
+            float ScreenScalingFactor = (float)PhysicalScreenHeight / (float)LogicalScreenHeight;
+            m_fltScreenScalingFactor_H = ScreenScalingFactor; // 1.25 = 125%
+
+            int LogicalScreenWidth = GetDeviceCaps(desktop, (int)DeviceCap.HORZRES);
+            int PhysicalScreenWidth = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPHORZRES);
+            ScreenScalingFactor = (float)PhysicalScreenWidth / (float)LogicalScreenWidth;
+            m_fltScreenScalingFactor_W = ScreenScalingFactor; // 1.25 = 125%
+        }
+        //--
+    }
+}
